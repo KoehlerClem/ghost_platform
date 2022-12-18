a51e8dd1f9ddcece317748e61121c1e75ebdfb23
Clemens Köhler <6440132+KoehlerClem@users.noreply.github.com>
2022-12-18 23:59:39 +0100

    infra

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ template_bucket=`aws cloudformation --profile $aws_profile describe-stacks --sta
 aws s3 --profile $aws_profile sync ./Infrastructure s3://$template_bucket
 
 # deploy infastructure and ci/cd pipelines
-aws cloudformation deploy --template-file CICD/codepipeline_app.yml --stack-name codepipeline-app --capabilities CAPABILITY_IAM --parameter-overrides ParameterKey=GitHubBranch,ParameterValue=$git_branch --profile $aws_profile &
+aws cloudformation deploy --template-file CICD/codepipeline_app.yml --stack-name codepipeline-app --capabilities CAPABILITY_IAM --parameter-overrides GitHubBranch=$git_branch --profile $aws_profile
 aws cloudformation deploy --template-file Infrastructure/ghost_infra.yml --stack-name ghost-blog --capabilities CAPABILITY_IAM --profile $aws_profile
-aws cloudformation deploy --template-file CICD/codepipeline_infra.yml --stack-name codepipeline-infra --capabilities CAPABILITY_IAM --parameter-overrides ParameterKey=GitHubBranch,ParameterValue=$git_branch --profile $aws_profile
+aws cloudformation deploy --template-file CICD/codepipeline_infra.yml --stack-name codepipeline-infra --capabilities CAPABILITY_IAM --parameter-overrides GitHubBranch=$git_branch --profile $aws_profile
