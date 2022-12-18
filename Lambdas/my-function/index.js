b27fe1c28ac1c7da42e70c6ddde8b5aca9687c8a
Clemens Köhler <6440132+KoehlerClem@users.noreply.github.com>
2022-12-18 16:32:29 +0100

    infra

--- Lambdas/my-function/index.js
+++ Lambdas/my-function/index.js
@@ -1,10 +1,10 @@
-const GhostAdminAPI = require('@tryghost/admin-api');
-const api = new GhostAdminAPI({
-    url: 'http://random-dev-domain.com',
-    version: "v5.0",
-    key: '6397ca72e0ea780001b49faf:ef47de045e25ae996d49d00e88c574cb9fc47c51a6e1a9d3b230321c82bf6cfd'
-});
 exports.handler =  async function(event, context) {
+    const GhostAdminAPI = require('@tryghost/admin-api');
+    const api = new GhostAdminAPI({
+        url: 'http://random-dev-domain.com',
+        version: "v5.0",
+        key: event['ghost_Admin_API_key']
+    });
     let posts = await api.posts.browse();
     console.log(`Got ${posts['meta']['pagination']['total']} posts`)
 
