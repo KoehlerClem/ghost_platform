6c92a0d4a4321fbf519aab04e5a876dc82c41109
Clemens Köhler <6440132+KoehlerClem@users.noreply.github.com>
2022-12-18 19:44:59 +0100

    infra

--- README.md
+++ README.md
@@ -12,15 +12,8 @@ https://docs.aws.amazon.com/dtconsole/latest/userguide/connections-update.html
     4. adds the env to the codedeploy template
 
 
-
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 674464173288.dkr.ecr.eu-central-1.amazonaws.com
-
-docker build -t ghost . 
-docker tag ghost 674464173288.dkr.ecr.eu-central-1.amazonaws.com/ghost-blog:latest
-
-docker push 674464173288.dkr.ecr.eu-central-1.amazonaws.com/ghost-blog:latest
-
-
+Clone the repository
+initialise the submodules: git submodule update --init --recursive
 Prequisites
 
 1. set db password
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,9 @@
 set -e
-git_branch=${1:?Please provide the git branch name which will be deployed to AWS, one branch per Account}
-aws_profile=${2:?Please provide the AWS profile which will be used to deploy the project}
-aws cloudformation deploy --template-file Infrastructure/ecr.yml --stack-name ghost-blog-ecr --profile $aws_profile
+PS4='Line ${LINENO}: '
+set -x
 
-# build docker image and push to ECR
-cd Ghost
-docker build -t ghost . 
-export ACCOUNT_ID="aws sts get-caller-identity --query Account --output text --profile $aws_profile"
-docker tag ghost $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/ghost-blog:latest
-aws ecr get-login-password --profile $aws_profile | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
-docker push $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/ghost-blog:latest
+aws_profile=${1:?Please provide branch and aws profile. Example: ./deploy.sh default main}
+git_branch=${2:?Please provide branch and aws profile. Example: ./deploy.sh default main}
 
 # push cloudformation templates to S3
 cd ..
@@ -17,6 +11,6 @@ aws cloudformation deploy --template-file Infrastructure/s3.yml --stack-name gho
 aws s3 --profile $aws_profile sync ./Infrastructure s3://ghost-blog-templates
 
 # deploy infastructure and ci/cd pipelines
-aws cloudformation deploy --template-file Infrastructure/ghost_infra.yml --stack-name ghost-blog --capabilities CAPABILITY_IAM --profile $aws_profile
+aws cloudformation deploy --template-file Infrastructure/ghost_infra.yml --stack-name ghost-blog --capabilities CAPABILITY_IAM --parameters ParameterKey=IsInitialDeploy,ParameterValue=true --profile $aws_profile
+aws cloudformation deploy --template-file CICD/codepipeline_app.yml --stack-name codepipeline-app --capabilities CAPABILITY_IAM --parameters ParameterKey=GitHubBranch,ParameterValue=$git_branch --profile $aws_profile &
 aws cloudformation deploy --template-file CICD/codepipeline_infra.yml --stack-name codepipeline-infra --capabilities CAPABILITY_IAM --parameters ParameterKey=GitHubBranch,ParameterValue=$git_branch --profile $aws_profile
-aws cloudformation deploy --template-file CICD/codepipeline_app.yml --stack-name codepipeline-app --capabilities CAPABILITY_IAM --parameters ParameterKey=GitHubBranch,ParameterValue=$git_branch --profile $aws_profile
