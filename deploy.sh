cfa2dd0c270fbe12410780694a375a9612cf8171
Clemens Köhler <6440132+KoehlerClem@users.noreply.github.com>
2022-12-18 16:08:49 +0100

    infra

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+set -e
+git_branch=${1:?Please provide git branch name which will be deployed to AWS on this account}
 aws cloudformation deploy --template-file Infrastructure/ecr.yml --stack-name ghost-blog-ecr
 
 # build docker image and push to ECR
@@ -11,8 +13,9 @@ docker push $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/ghost-blog:latest
 # push cloudformation templates to S3
 cd ..
 aws cloudformation deploy --template-file Infrastructure/s3.yml --stack-name ghost-blog-s3
-aws s3 sync ./Infrastructure s3://ghost-blog-s3
+aws s3 sync ./Infrastructure s3://ghost-blog-templates
 
-# deploy infastructure pipeline
-aws cloudformation deploy --template-file Infrastructure/codepipeline_infra.yml --stack-name codepipeline-infra --capabilities CAPABILITY_IAM
-# aws cloudformation deploy --template-file Infrastructure/ghost_infra.yml --stack-name ghost-blog --capabilities CAPABILITY_IAM
+# deploy infastructure and ci/cd pipelines
+aws cloudformation deploy --template-file Infrastructure/ghost_infra.yml --stack-name ghost-blog --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file CICD/codepipeline_infra.yml --stack-name codepipeline-infra --capabilities CAPABILITY_IAM --parameters ParameterKey=GitHubBranch,ParameterValue=$git_branch
+aws cloudformation deploy --template-file CICD/codepipeline_app.yml --stack-name codepipeline-app --capabilities CAPABILITY_IAM --parameters ParameterKey=GitHubBranch,ParameterValue=$git_branch
